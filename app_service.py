aad66e4711a41cdac5a83fe1ed4c8f8a18601656
Tanny Chavez Esparza <tanchavez1@gmail.com>
2022-05-30 10:49:29 -0700

    added extra endpoints

--- app_service.py
+++ app_service.py
@@ -1,5 +1,6 @@
 import logging
 from fastapi import FastAPI
+import requests
 import subprocess
 import uvicorn
 
@@ -19,31 +20,47 @@ app = FastAPI(openapi_url='/api/app_service/openapi.json',
               redoc_url='/api/app_service/redoc')
 
 
+@app.post(URL_PREFIX + '/reverse', tags=['applications'])
+def reverse_message(message: str):
+    rev_msg = subprocess.check_output(f'python3 app1.py \"{message}\"', shell=True).decode()
+    logger.info(f'Output of APP1: {rev_msg}')
+    return rev_msg
+
+
+@app.post(URL_PREFIX + '/convert', tags=['applications'])
+def convert_message(message: str):
+    conv_msg = subprocess.check_output(f'python3 app2.py \"{message}\"', shell=True).decode()
+    logger.info(f'Output of APP2: {conv_msg}')
+    return conv_msg
+
+
+@app.post(URL_PREFIX + '/save', tags=['applications'])
+def save_message(message: str):
+    out = subprocess.check_output(f'python3 app3.py \"{message}\"', shell=True).decode()
+    logger.info(f'Output of APP3: {out}')
+    return out
+
+
 @app.post(URL_PREFIX + '/file_service', tags=['applications'])
-def full_service(filename: str):
+def file_service(filename: str):
     # Load local file
     f = open(filename, "r")
     contents = f.read().splitlines()  # each line as an element in a list
     logger.info(f'Received: {contents}')
-    # Start pipeline
-    output_app1 = subprocess.check_output(f'python3 app1.py \"{contents[0]}\"', shell=True).decode()
-    logger.info(f'Output of APP1: {output_app1}')
-    output_app2 = subprocess.check_output(f'python3 app2.py \"{output_app1}\"', shell=True).decode()
-    logger.info(f'Output of APP2: {output_app2}')
-    output_app3 = subprocess.check_output(f'python3 app3.py \"{output_app2}\"', shell=True).decode()
-    logger.info(f'Output saved locally as {output_app3}')
-    return output_app3
+    out1 = requests.post(f'http://localhost:8080{URL_PREFIX}/reverse?message={contents[0]}').json()
+    out2 = requests.post(f'http://localhost:8080{URL_PREFIX}/convert?message={out1}').json()
+    out3 = requests.post(f'http://localhost:8080{URL_PREFIX}/save?message={out2}').json()
+    return out3
 
 
 @app.post(URL_PREFIX + '/alt_service', tags=['applications'])
-def alt_service(fcontents: str):
-    # Start pipeline
-    output_app1 = subprocess.check_output(f'python3 app1.py \"{fcontents}\"', shell=True).decode()
-    logger.info(f'Output of APP1: {output_app1}')
-    output_app2 = subprocess.check_output(f'python3 app2.py \"{output_app1}\"', shell=True).decode()
-    logger.info(f'Output of APP2: {output_app2}')
-    out = f'Output of APP1: {output_app1}\nOutput of APP2: {output_app2}'
-    return out
+def online_service(fcontents: str):
+    logger.info(f'Received: {fcontents}')
+    out1 = requests.post(f'http://localhost:8080{URL_PREFIX}/reverse?message={fcontents}').json()
+    out2 = requests.post(f'http://localhost:8080{URL_PREFIX}/convert?message={out1}').json()
+    out3 = requests.post(f'http://localhost:8080{URL_PREFIX}/save?message={out2}').json()
+    return out3
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8080)
